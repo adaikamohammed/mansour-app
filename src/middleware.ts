922673fc1d7dd35a5271d63d0668d6b91ad45e97
Adaika Mohammed <118262120+adaikamohammed@users.noreply.github.com>
2026-04-30 09:17:03 +0100

    ada14

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest } from 'next/server';
-import { proxy, config as proxyConfig } from './proxy';
-
-export async function middleware(req: NextRequest) {
-  return await proxy(req);
-}
-
-export const config = proxyConfig;
